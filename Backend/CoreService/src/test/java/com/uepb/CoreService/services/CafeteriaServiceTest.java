4e00ce08291084648311c0abff50348b5abdfec7
AndreVinnis <andrev.macambira@gmail.com>
2026-06-22 17:18:24 -0300

    fix: consertando testes para passarem no workflow

--- Backend/CoreService/src/test/java/com/uepb/CoreService/services/CafeteriaServiceTest.java
+++ Backend/CoreService/src/test/java/com/uepb/CoreService/services/CafeteriaServiceTest.java
@@ -14,12 +14,16 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 class CafeteriaServiceTest {
 
