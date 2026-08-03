aee6e0dc37dbf7757869490a8b3969c4bce0ac27
AndreVinnis <andrev.macambira@gmail.com>
2026-08-03 13:45:13 -0300

    feat: criando as classes base de PaymentService

---
+++ Backend/PaymentService/src/main/java/com/uepb/PaymentService/domain/Purchase.java
@@ -0,0 +1,63 @@
+package com.uepb.PaymentService.domain;
+
+import com.uepb.PaymentService.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity
+@Table(name = "purchases")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String cafeteriaId;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private String abacatePayId;
+
+    @Column(nullable = false)
+    private String qrCode;
+
+    @Column(nullable = false)
+    private String qrCodeImage;
+
+    @Column(name = "expired_at", nullable = false, updatable = false)
+    private Instant expiredAt;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now();
+    }
+}
---
+++ Backend/PaymentService/src/main/java/com/uepb/PaymentService/enums/Status.java
@@ -0,0 +1,8 @@
+package com.uepb.PaymentService.enums;
+
+public enum Status {
+
+    PENDING,
+    PAID,
+    EXPIRED
+}
